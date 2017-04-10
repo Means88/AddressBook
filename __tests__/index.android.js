7a790cb75be370decd8c8231306a00d62625c302
means88 <kk2505061@gmail.com>
2017-04-10 10:52:16 +0800

    fix test

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -5,6 +5,8 @@ import Index from '../index.android.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('mobx-react/native', () => require('mobx-react/custom'));
+
 it('renders correctly', () => {
   const tree = renderer.create(
     <Index />
--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -5,6 +5,8 @@ import Index from '../index.ios.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('mobx-react/native', () => require('mobx-react/custom'));
+
 it('renders correctly', () => {
   const tree = renderer.create(
     <Index />
